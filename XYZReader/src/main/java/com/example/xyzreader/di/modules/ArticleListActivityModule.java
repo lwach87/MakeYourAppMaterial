602ace543e7f59583defbf88483bba89b4f0b4d9
Łukasz Wachowski <lwach87@gmail.com>
2018-11-07 22:09:43 +0100

    AppComponents added

--- XYZReader/src/main/java/com/example/xyzreader/di/modules/ArticleListActivityModule.java
+++ XYZReader/src/main/java/com/example/xyzreader/di/modules/ArticleListActivityModule.java
@@ -1,6 +1,5 @@
 package com.example.xyzreader.di.modules;
 
-import com.example.xyzreader.di.ActivityScope;
 import com.example.xyzreader.ui.Adapter;
 import com.example.xyzreader.ui.ArticleListActivity;
 import com.squareup.picasso.Picasso;
@@ -10,15 +9,8 @@ import dagger.Provides;
 @Module
 public class ArticleListActivityModule {
 
-  private ArticleListActivity listActivity;
-
-  public ArticleListActivityModule(ArticleListActivity listActivity) {
-    this.listActivity = listActivity;
-  }
-
   @Provides
-  @ActivityScope
-  public Adapter provideAdapter(Picasso picasso) {
+  public Adapter provideAdapter(Picasso picasso, ArticleListActivity listActivity) {
     return new Adapter(picasso, listActivity);
   }
 }
